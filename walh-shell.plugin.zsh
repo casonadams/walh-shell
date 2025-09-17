a59120f60198bf22de6e026a304160010ae8bc89
Cason Adams <casonadams@gmail.com>
2025-09-16 21:04:13 -0600

    fix tmux and vim issues

--- walh-shell.plugin.zsh
+++ walh-shell.plugin.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
+# Do not run if inside Vim or Neovim
+if [[ -n "$VIMRUNTIME" || -n "$VIM" || -n "$NVIM" ]]; then
+  return
+fi
+
 WALH_SHELL=$(dirname "${(%):-%x}")
 
 [ -n "$PS1" ] \
     && [ -s "${WALH_SHELL}/profile_helper.sh" ] \
-    && eval "$("${WALH_SHELL}/profile_helper.sh")"
+    && eval "$(${WALH_SHELL}/profile_helper.sh)"
